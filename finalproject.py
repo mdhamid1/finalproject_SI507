a6863a503adfcb68ff7ebcd437c5fffc5901a0c9
Muhammed Hamid <muhammedhamid@hotmail.com>
2021-09-12 03:07:02 -0400

    pushing updated code for final project

--- finalproject.py
+++ finalproject.py
@@ -3,7 +3,12 @@ import time
 import hashlib
 import json
 from bs4 import BeautifulSoup
+import sqlite3
+import plotly.graph_objects as go
 
+DBNAME = 'marvel_characters.db'
+connection = sqlite3.connect(DBNAME)
+cursor = connection.cursor()
 
 CACHE_FILENAME_CHARACTERS = "characters_cache.json"
 
@@ -45,7 +50,7 @@ def save_cache(cache_dict, cache_filename):
     dumped_json_cache = json.dumps(cache_dict, sort_keys=True, indent=0)
     fw = open(cache_filename,"w")
     fw.write(dumped_json_cache)
-    fw.close() 
+    fw.close()
 
 def get_event_info(character_name):
 
@@ -99,6 +104,7 @@ def extract_characters():
                 character_dict[character_title] = {}
                 character_dict[character_title]['link'] = base_url+character_info_link
                 character_dict[character_title]['powers'] = {}
+                character_dict[character_title]['events'] = [0]
             if character_counts != 2592:
                 offset += 36
             save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
@@ -154,11 +160,163 @@ def check_for_power_page(response_object):
     return soup
 
 
-character_dict = extract_character_info()
 
-for key in character_dict.keys():
-    print('storing events....')
-    result = get_event_info(key)
-    print(result[-1].keys())
-    # character_dict[key]['events'] = result['comics']['events']
-    # save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
+
+def dbconn(query):
+    connection = sqlite3.connect(DBNAME)
+    cursor = connection.cursor()
+    result = cursor.execute(query).fetchall()
+    connection.close()
+    return result
+
+
+class Character:
+    def __init__(self, name, info_link, powers, events):
+        self.name = name
+        self.info_link = info_link
+        self.powers = powers
+
+        if events != [[]] or []:
+            self.events = events['items']
+        else:
+            self.events = "This character is not mentioned in any events"
+
+    def info(self):
+
+        return f'''
+        Name: {self.name}
+        Character Information: {self.info_link}
+        Powers: {self.powers}
+        Events: {self.events}
+        '''
+
+
+
+
+character_dict = extract_character_info()
+character_object_list = []
+character_dict = open_cache(CACHE_FILENAME_CHARACTERS)
+
+
+def create_database_table():
+
+    q1 = '''CREATE TABLE "CharacterInfo" (
+    "id"	INTEGER NOT NULL,
+    "Name"	TEXT NOT NULL,
+    "Link"	TEXT,
+    "Durability"	INTEGER,
+    "Energy"	INTEGER,
+    "Fighting Skills"	INTEGER,
+    "Intelligence"	INTEGER,
+    "Speed"	INTEGER,
+    "Strength"	INTEGER,
+    PRIMARY KEY("id" AUTOINCREMENT));'''
+
+    cursor.execute(q1)
+
+
+    q2 = '''CREATE TABLE "CharacterEvents" (
+    "id"	INTEGER NOT NULL,
+    "infoId"	INTEGER,
+    "Events"	TEXT,
+    PRIMARY KEY("id" AUTOINCREMENT),
+    FOREIGN KEY("infoId") REFERENCES "CharacterInfo"("id"));'''
+
+    cursor.execute(q2)
+
+
+def populate_database():
+
+    sql_info = "INSERT INTO CharacterInfo (`Name`, `Link`, `Durability`, `Energy`, `Fighting Skills`, `Intelligence`, `Speed`, `Strength`) VALUES (?,?,?,?,?,?,?,?)"
+    sql_events = "INSERT INTO CharacterEvents (`infoId`, `Events`) VALUES (?,?)"
+
+    events_list = []
+    if character_dict[key]['powers'] != "powers not listed":
+            val = (key, character_dict[key]['link'], character_dict[key]['powers']['durability'], character_dict[key]['powers']['energy'], character_dict[key]['powers']['fighting skills'], character_dict[key]['powers']['intelligence'], character_dict[key]['powers']['speed'], character_dict[key]['powers']['strength'])
+    else:
+        val = (key, character_dict[key]['link'], None, None, None, None, None, None)
+    cursor.execute(sql_info, val)
+    last_id = cursor.lastrowid
+
+    if character_dict[key]['events'] != [[]]:
+        for event_dict in character_dict[key]['events']['items']:
+            if character_dict[key]['events']['items'] != []:
+                val = event_dict["name"]
+                events_list.append(val)
+            else:
+                val = None
+                events_list.append(val)
+    for item in events_list:
+        cursor.execute(sql_events, (last_id,item))
+
+
+
+def interactive_prompt():
+
+    running = True
+    while running:
+        character_input = input("Please enter the name of a Marvel character: ")
+        if character_input.lower() == 'exit':
+            print("Thanks for using the Marvel Character Database!" + "\n")
+            running = False
+        else:
+            character_input = character_input.title()
+            character_input = character_input.replace("bar", "")
+
+            for character in character_object_list:
+                if character_input in character.name:
+                    name_var = character.name
+                    if "'" in name_var:
+                        name_var = name_var.replace("'", "''")
+
+                    query = f"select * from CharacterInfo where Name = '{name_var}'"
+                    query_result = dbconn(query)
+                    print(character.info())
+                    if character.powers != "powers not listed":
+                        x_list = list(character.powers.keys())
+                        y_list = []
+                        for power_str in character.powers.values():
+                            power_int = int(power_str)
+                            y_list.append(power_int)
+
+                        bar_data = go.Bar(x=x_list, y=y_list)
+                        basic_layout = go.Layout(title=f"{character.name}")
+                        fig = go.Figure(data=bar_data, layout=basic_layout)
+                        fig.show()
+                else:
+                    continue
+
+
+#### Only run below function if running the script for the first time and the marvel_characters.db file does not exist in the directory ####
+#create_database_table()
+
+for x, key in enumerate(character_dict.keys()):
+    character_dict = open_cache(CACHE_FILENAME_CHARACTERS)
+    char_obj = Character(key, character_dict[key]['link'], character_dict[key]['powers'], character_dict[key]['events'])
+    character_object_list.append(char_obj)
+
+    if character_dict[key]['events'] == [0]:
+        print("running....")
+        result = get_event_info(key)
+        if result != [] and result[0]['events'] not in character_dict[key].values():
+            print('storing events....')
+            character_dict[key]['events'] = result[0]['events']
+            save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
+        else:
+             character_dict[key]['events'] = []
+             save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
+
+
+    #### Only run below function if running the script for the first time and the marvel_characters.db file does not exist in the directory ####
+    # populate_database()
+
+
+
+connection.commit()
+cursor.close()
+connection.close()
+
+
+if __name__ == "__main__":
+    interactive_prompt()
+

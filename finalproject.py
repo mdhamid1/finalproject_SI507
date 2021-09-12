66bb791b7f1cebfda1c5173190a3b9d35816258c
Muhammed Hamid <76439219+mdhamid1@users.noreply.github.com>
2021-09-12 17:33:42 -0400

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -54,6 +54,22 @@ def save_cache(cache_dict, cache_filename):
 
 def get_event_info(character_name):
 
+    '''
+    Create the necessary API parameters and call the Marvel API"
+
+    Parameters
+    ----------
+    character_name: string
+        string containing character name
+
+    Returns
+    -------
+    dict
+        contains information about the events this character has been involved in, in the comics
+
+    '''
+
+
     base_url_api = "http://gateway.marvel.com/v1/public/characters"
     api_key_public = "47afdd489562831d49d7ee079fedbc69"
     api_key_private = "8735fd6c7c994d79c2b990c348454dfbbb668fcd"
@@ -75,15 +91,30 @@ def get_event_info(character_name):
     response_json = response.json()
     response_results_events = response_json['data']['results']
 
-    #print(response_results[0]['events']['items'])
-
     return response_results_events
 
 
 
-#Beautiful soup to scrape webpage: 
+#Beautiful soup to scrape webpage:
 
 def extract_characters():
+
+    '''
+    Extracts the name of all the Marvel characters on the Characters page of the Marvel Website.
+    Adds the character name to the overall summary dictionary for all the Marvel characters on the website.
+    Initializes some other keys within the dictonary.
+
+    Parameters
+    ----------
+    None
+
+
+    Returns
+    -------
+    character_dict: dict
+        Adds the character name to the overall summary dictionary for all the Marvel characters on the website
+
+    '''
 
     offset = 0
     limit = 10
@@ -114,6 +145,22 @@ def extract_characters():
 
 def extract_character_info():
 
+    '''
+    Extracts information about all Marvel characters whose name is present in characters_dict
+    and adds this information into characters_dict.
+
+    Parameters
+    ----------
+    None
+
+
+    Returns
+    -------
+    character_dict: dict
+        Overall summary dictionary for Marvel characters
+
+    '''
+
     character_dict = extract_characters()
 
     for key in character_dict.keys():
@@ -142,6 +189,24 @@ def extract_character_info():
 
 def check_for_power_page(response_object):
 
+    '''
+    Checks to ensure that for each character there is a page that contains the powers of the character.
+    At times this page is not directly found; if this is the case, this function navigates to the appropriate
+    page to ensure that the scraping is being done properly.
+
+    Parameters
+    ----------
+    reponse_object
+        contains response from web server that will be scraped
+
+
+    Returns
+    -------
+    soup
+        returns the parserable object that will be used to extract information about the Marvel character in question
+
+    '''
+
     base_url = 'https://www.marvel.com'
 
     soup = BeautifulSoup(response_object.text, 'html.parser')
@@ -163,6 +228,22 @@ def check_for_power_page(response_object):
 
 
 def dbconn(query):
+    '''
+    Establishes a connection with the database and passes a query into the database
+
+    Parameters
+    ----------
+    query: string
+        String consisting of the SQL query of interest
+
+
+    Returns
+    -------
+    result: list
+        returns the query result in the form of a list
+
+    '''
+
     connection = sqlite3.connect(DBNAME)
     cursor = connection.cursor()
     result = cursor.execute(query).fetchall()
@@ -171,6 +252,23 @@ def dbconn(query):
 
 
 class Character:
+    '''
+    Creates a Character object from the passed in attributes
+
+    Parameters
+    ----------
+    name: string, name of character
+    info_link: string, url to Marvel website containing character info
+    powers: dict, contains powers names and values for the Marvel character
+    events: dict, contains info about the events that the Marvel character has been involved in
+
+
+    Returns
+    -------
+    None
+
+    '''
+
     def __init__(self, name, info_link, powers, events):
         self.name = name
         self.info_link = info_link
@@ -199,6 +297,19 @@ character_dict = open_cache(CACHE_FILENAME_CHARACTERS)
 
 
 def create_database_table():
+    '''
+    Creates two tables in database via two queries
+
+    Parameters
+    ----------
+    None
+
+
+    Returns
+    -------
+    None
+
+    '''
 
     q1 = '''CREATE TABLE "CharacterInfo" (
     "id"	INTEGER NOT NULL,
@@ -226,6 +337,20 @@ def create_database_table():
 
 
 def populate_database():
+    '''
+    Populates both tables created earlier with data stored in the character_dict dictionary
+
+    Parameters
+    ----------
+    query: string
+        String consisting of the SQL query of interest
+
+
+    Returns
+    -------
+    None
+
+    '''
 
     sql_info = "INSERT INTO CharacterInfo (`Name`, `Link`, `Durability`, `Energy`, `Fighting Skills`, `Intelligence`, `Speed`, `Strength`) VALUES (?,?,?,?,?,?,?,?)"
     sql_events = "INSERT INTO CharacterEvents (`infoId`, `Events`) VALUES (?,?)"
@@ -252,6 +377,18 @@ def populate_database():
 
 
 def interactive_prompt():
+    '''
+    Executes interactive prompt that allows the user to interact with the program
+
+    Parameters
+    ----------
+    None
+
+
+    Returns
+    -------
+    None
+    '''
 
     running = True
     while running:
@@ -287,36 +424,38 @@ def interactive_prompt():
                     continue
 
 
-#### Only run below function if running the script for the first time and the marvel_characters.db file does not exist in the directory ####
-#create_database_table()
-
-for x, key in enumerate(character_dict.keys()):
-    character_dict = open_cache(CACHE_FILENAME_CHARACTERS)
-    char_obj = Character(key, character_dict[key]['link'], character_dict[key]['powers'], character_dict[key]['events'])
-    character_object_list.append(char_obj)
-
-    if character_dict[key]['events'] == [0]:
-        print("running....")
-        result = get_event_info(key)
-        if result != [] and result[0]['events'] not in character_dict[key].values():
-            print('storing events....')
-            character_dict[key]['events'] = result[0]['events']
-            save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
-        else:
-             character_dict[key]['events'] = []
-             save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
-
-
-    #### Only run below function if running the script for the first time and the marvel_characters.db file does not exist in the directory ####
-    # populate_database()
-
-
-
-connection.commit()
-cursor.close()
-connection.close()
 
 
 if __name__ == "__main__":
+
+    #### Only run below function if running the script for the first time and the marvel_characters.db file does not exist in the directory ####
+    create_database_table()
+
+
+    for x, key in enumerate(character_dict.keys()):
+        character_dict = open_cache(CACHE_FILENAME_CHARACTERS)
+        char_obj = Character(key, character_dict[key]['link'], character_dict[key]['powers'], character_dict[key]['events'])
+        character_object_list.append(char_obj)
+
+        if character_dict[key]['events'] == [0]:
+            print("running....")
+            result = get_event_info(key)
+            if result != [] and result[0]['events'] not in character_dict[key].values():
+                print('storing events....')
+                character_dict[key]['events'] = result[0]['events']
+                save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
+            else:
+                character_dict[key]['events'] = []
+                save_cache(character_dict, CACHE_FILENAME_CHARACTERS)
+
+
+        #### Only run below function if running the script for the first time and the marvel_characters.db file does not exist in the directory ####
+        populate_database()
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
     interactive_prompt()
 
